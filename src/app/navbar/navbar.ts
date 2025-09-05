3c571e518055a09b14b974a50e042ce297995d22
Abhijit Surve <surveabhijit2002@gmail.com>
2025-09-06 00:13:51 +0530

    navbar implemented

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+
+}
