d1e559671bbea70857adb8d76f3ab534bcb5137d
Abhijit Surve <surveabhijit2002@gmail.com>
2025-10-26 22:35:31 +0530

    pushed env file

---
+++ src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyAa65jPm8ZAVQYaIJyElImARtKBhh_fPp8",
+      authDomain: "fixpass-db60b.firebaseapp.com",
+      projectId: "fixpass-db60b",
+      storageBucket: "fixpass-db60b.appspot.com",
+      messagingSenderId: "527863943252	",
+      appId: "1:527863943252:web:dc4a61424425aec3d55dc1"
+    }
+  };
+  
